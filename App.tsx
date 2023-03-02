90fd1b39306129e1c97a12067043e1904548168d
codewithshamim <shamimislamonline@gmail.com>
2023-03-03 00:42:27 +0600

    update

--- App.tsx
+++ App.tsx
@@ -112,7 +112,7 @@ const App = () => {
             onChangeText={text => setToken(text)}></TextInput>
           <Button
             title="Connect"
-            style={styles.button}
+            // style={styles.button}
             onPress={_onConnectButtonPress}></Button>
         </View>
       )}
@@ -169,4 +169,75 @@ const App = () => {
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  callContainer: {
+    flex: 1,
+    position: "absolute",
+    bottom: 0,
+    top: 0,
+    left: 0,
+    right: 0
+  },
+  welcome: {
+    fontSize: 30,
+    textAlign: "center",
+    paddingTop: 40
+  },
+  input: {
+    height: 50,
+    borderWidth: 1,
+    marginRight: 70,
+    marginLeft: 70,
+    marginTop: 50,
+    textAlign: "center",
+    backgroundColor: "white"
+  },
+  button: {
+    marginTop: 100
+  },
+  localVideo: {
+    flex: 1,
+    width: 125,
+    height: 200,
+    position: "absolute",
+    right: 10,
+    bottom: 400,
+    borderRadius: 2,
+    borderColor: '#4e4e4e'
+  },
+  remoteGrid: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap"
+  },
+  remoteVideo: {
+    width: '100%',
+    height: '100%'
+  },
+  optionsContainer: {
+    position: "absolute",
+    left: 0,
+    bottom: 0,
+    right: 0,
+    height: 100,
+    // backgroundColor: "blue",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: 'center'
+  },
+  optionButton: {
+    width: 60,
+    height: 60,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 100 / 2,
+    backgroundColor: "grey",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
+
